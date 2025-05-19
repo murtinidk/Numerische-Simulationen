bb55f85bc69bd198b262a075660ec73ebd8d6488
Abhay Ram <abhay.ram@student.tugraz.at>
2025-05-19 10:07:58 +0200

    remove example config

--- Src/data/node.py
+++ Src/data/node.py
@@ -4,14 +4,14 @@ class Node:
         self.realX = realX
         self.realY = realY
         self.dirichletBoundary = None
-        if self.index == 1:
-            self.dirichletBoundary = 0
+        # if self.index == 1:
+        #     self.dirichletBoundary = 0
         #if self.index == 8:
             #self.dirichletBoundary = 2
         self.rightVonNeumannBoundary = None
         self.belowVonNeumannBoundary = None
-        if self.index == 1:
-            self.belowVonNeumannBoundary = 1
+        # if self.index == 1:
+        #     self.belowVonNeumannBoundary = 1
         self.result = None
 
     def SetResult(self, result: float):
